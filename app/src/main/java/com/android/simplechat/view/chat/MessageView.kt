c9c978b22e980a41fea51f66d0b1511651c037e6
Lishan Qiu <yozyyyqls@gmail.com>
2022-11-14 22:24:01 +0800

    add(view, chat): add feature of showing message flow in chat page.

---
+++ app/src/main/java/com/android/simplechat/view/chat/MessageView.kt
@@ -0,0 +1,8 @@
+package com.android.simplechat.view.chat
+
+import android.content.Context
+import android.widget.LinearLayout
+
+open class MessageView constructor(
+    context: Context
+) : LinearLayout(context)
